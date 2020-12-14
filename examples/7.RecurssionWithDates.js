276ecf2aae67d28beabe6cdf36abfc69695aa192
Mario Galea <Mario.s.galea@gmail.com>
2020-12-14 01:36:21 +0100

    Added Recurssion with Dates example

--- examples/7.RecurssionWithDates.js
+++ examples/7.RecurssionWithDates.js
@@ -2,25 +2,35 @@ var RuleEngine = require('../rules-engine');
 /* Sample Rule to increment an attribute if its less than 10*/
 var rule = {
     "condition": function(R) {
-        R.when(this.someval < 5);
+        var Today=  Date.now();
+        R.when(Date.parse(this.someDate) < Today);
     },
-    "consequence": function(R) {
-        console.log(this.someval++, " : incrementing again till 5");
+    "action": function(R) {
+        var startDate = new Date(this.someDate);
+        console.log(this.someDate, " : incrementing a day till Today");
+        // seconds * minutes * hours * milliseconds = 1 day 
+        var day = 60 * 60 * 24 * 1000;
+        var endDate = new Date(startDate.getTime() + day);
+        this.someDate=endDate.toISOString();
         R.restart();
     },
-    "else": function (R) {
-        console.log(this.someval, " : Its 10!");
+    "else" : function (R) {
+        var startDate = new Date(this.someDate);
+        var day = 60 * 60 * 24 * 1000;
+        var endDate = new Date(startDate.getTime() - day);
+        this.someDate = endDate.toISOString();
         R.stop();
     }
 };
 /* Creating Rule Engine instance and registering rule */
-var R = new RuleEngine(null,{ debug: true });
-R.register(rule);
+var R = new RuleEngine(rule,{ignoreFactChanges:true, debug: false });
 /* some val is 0 here, rules will recursively run till it becomes 10.
 This just a mock to demo the restart feature. */
 var fact = {
-    "someval": 0
+    "someDate": '2020-10-13'
 };
+console.log(Date.parse(fact.someDate))
+
 R.execute(fact, function(data) {
-    console.log("Finished with value", data.someval);
+    console.log("Finished with value", data.someDate);
 });
